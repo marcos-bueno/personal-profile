6905e3a97f46a76ab1a7508cd0d5d173385d3563
Marcos <marcos_btv@live.com>
2020-10-16 19:57:37 -0300

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -122,26 +122,20 @@ export default function Index() {
             </div>
           </div>
           <div className="formBx">
-          <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+            <form name="contact" method="POST" data-netlify="true">
+              <p>
+                <input type="text" name="name" /> 
+              </p>
+              <p>
+                <input type="email" name="email" />
+              </p>
+              <p>
+                <textarea name="message"></textarea>
+              </p>
+              <p>
+                <button type="submit">Enviar</button>
+              </p>
+            </form>
           </div>
         </div>
       </section>
